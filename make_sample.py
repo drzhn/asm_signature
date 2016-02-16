ce6d0d2e17019b2357d2c40269e03395c88cc907
drzhn <kleverteo@gmail.com>
2016-02-16 17:34:18 +0300

    improved parsing

--- make_sample.py
+++ make_sample.py
@@ -5,7 +5,7 @@ listdir = os.listdir("sample_bin")
 # print(dir)
 # print(os.listdir("sample/"+dir[0]))
 path = os.path.abspath("sample_bin") + "/"
-
+devnull = open("/dev/null", "w")
 if not os.path.exists("sample"):
     os.makedirs("sample")
 outpath = os.path.abspath("sample") + "/"
@@ -18,13 +18,11 @@ for i in range(len(listdir)):
     print(name)
     if not os.path.exists(outpath+name):
         os.makedirs(outpath+name)
-
     for j in range(len(samples)):
-        devnull = open("/dev/null", "w")
         retcode = subprocess.call(
             ["objdump -D -M intel " + fullpath + samples[j] + " > asm.txt"],
             shell=True, stderr=devnull)
         parse_file(samples[j],name)
-        devnull.close()
-        # print("     objdump -D -M intel "+fullpath+"/"+samples[j]+" > /home/klever/Projects/python/asm_signature/asm.txt")
         print("    " + fullpath + "/" + samples[j])
+
+devnull.close()
